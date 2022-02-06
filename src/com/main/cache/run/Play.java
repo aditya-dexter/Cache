94e15c0658dc66ed35cc0ee7d527041c997ddeb0
Dexter <ankush@Ankushs-MacBook-Pro.local>
2022-02-06 23:33:23 +0530

    config

--- src/com/main/cache/run/Play.java
+++ src/com/main/cache/run/Play.java
@@ -10,54 +10,58 @@ import java.util.Scanner;
 
 public class Play {
     public static void main(String args[]){
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Select cache type..");
-        System.out.println("1. Least Recently Used (LRU)");
-        System.out.println("2. Most Recently Used (MRU)");
-        System.out.println("3. Least Frequently Used (LFU)");
-        System.out.println("4. Most Frequently Used (MFU)");
-        System.out.println("9. To execute all JUnits");
+        try {
+            Scanner scanner = new Scanner(System.in);
+            System.out.println("Select cache type..");
+            System.out.println("1. Least Recently Used (LRU)");
+            System.out.println("2. Most Recently Used (MRU)");
+            System.out.println("3. Least Frequently Used (LFU)");
+            System.out.println("4. Most Frequently Used (MFU)");
+            System.out.println("9. To execute all JUnits");
 
-        int code = scanner.nextInt();
+            int code = scanner.nextInt();
 
-        if(code == 9){
-            Result result= JUnitCore.runClasses(TestSuite.class);
-            System.exit(0);
-        }
-
-        System.out.println("Select capacity");
-
-        int capacity = scanner.nextInt();
-        EvictionPolicy evictionPolicy = EvictionPolicy.getByCode(code);
-        Cache<Integer, Integer> cache = Cache.create(evictionPolicy, capacity);
-
-        System.out.println("<Integer, Integer> "+ evictionPolicy+" Cache created, please select an action");
-
-        while(true){
-            System.out.println("1. Put element");
-            System.out.println("2. Get element");
-            System.out.println("0. Exit");
-
-            int action = scanner.nextInt();
-
-            if(action == 1){
-                System.out.print("Insert key=");
-                int key = scanner.nextInt();
-                System.out.print("Insert value=");
-                int value = scanner.nextInt();
-                cache.put(key,value);
-            }
-
-            if(action == 2){
-                System.out.print("Get key=");
-                int key = scanner.nextInt();
-                cache.get(key);
-            }
-
-            if(action == 0){
+            if (code == 9) {
+                Result result = JUnitCore.runClasses(TestSuite.class);
                 System.exit(0);
             }
-        }
 
+            System.out.println("Select capacity");
+
+            int capacity = scanner.nextInt();
+            EvictionPolicy evictionPolicy = EvictionPolicy.getByCode(code);
+            Cache<Integer, Integer> cache = Cache.create(evictionPolicy, capacity);
+
+            System.out.println("<Integer, Integer> " + evictionPolicy + " Cache created, please select an action");
+
+            while (true) {
+                System.out.println("1. Put element");
+                System.out.println("2. Get element");
+                System.out.println("0. Exit");
+
+                int action = scanner.nextInt();
+
+                if (action == 1) {
+                    System.out.print("Insert key=");
+                    int key = scanner.nextInt();
+                    System.out.print("Insert value=");
+                    int value = scanner.nextInt();
+                    cache.put(key, value);
+                }
+
+                if (action == 2) {
+                    System.out.print("Get key=");
+                    int key = scanner.nextInt();
+                    cache.get(key);
+                }
+
+                if (action == 0) {
+                    System.exit(0);
+                }
+            }
+        }catch (Exception e){
+            System.out.println(e.getMessage());
+            System.out.println("Invalid input, Exiting............please rerun the program");
+        }
     }
 }
